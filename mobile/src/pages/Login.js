415bbdfd2e7be7bf55b64135076e502fdd351bc7
Thiago Fonseca <thiago.f.t@hotmail.com>
2019-10-16 02:53:17 -0300

    Implementing Login functionalities, SpotList component and it's functionalities

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, TouchableOpacity, Platform, Image, TextInput, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, TouchableOpacity, Platform, Image, TextInput, StyleSheet, Text } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('List');
+            }
+        })
+    },[]);
+
+    async function handleSubmit(event) {
+        const response = await api.post('/sessions', {
+            email
+        })
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
     return (
-        <View enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
+        <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
             <Image source={logo} />
 
             <View style={styles.form}>
@@ -17,6 +43,8 @@ export default function Login() {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
                 <Text style={styles.label} >TECNOLOGIAS *</Text>
                 <TextInput 
@@ -25,13 +53,15 @@ export default function Login() {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                     <Text style={styles.buttonText}>Encontrar spots</Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
